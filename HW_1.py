ad019c09cdb9aba64b8a09836b27d5f8f92b921c
user <old.91@bk.ru>
2023-12-07 22:47:23 +0300

    1

--- HW_1.py
+++ HW_1.py
@@ -1,12 +1,9 @@
 import subprocess
 
 
-#result = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIPE, encoding="utf=8")
-#print(result)
-#out = result.stdout
-#print(out)
 
-def test_func(command, text):
+
+def chek_func(command, text):
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding="utf=8")
     out = result.stdout
     if result.returncode == 0 and text in out:
--- test_1.py
+++ test_1.py
@@ -1,11 +1,17 @@
 import pytest
-
-from HW_1 import test_func
+from HW_1 import chek_func
+folder_in = "/home/user/folder_in"
+folder_out = "/home/user/folder_out"
+folder_ex = "/home/user/folder_ex"
 def test_step1():
-
-    assert test_func("cd /home/user/folder_in; 7z a /home/user/folder_out/archive_1",  "ever is ok")
+    assert chek_func(f"cd {folder_in}; 7z a {folder_out}/archive_1",  "Everything is Ok")
 
 def test_step2():
-    assert test_func("cd /home/user/folder_out; 7z d archive_1", "ever is ok")
+    assert chek_func(f"cd {folder_out}; 7z d archive_1", "Everything is Ok")
+
+def test_step3():
+    assert chek_func(f"cd {folder_out}; 7z x {folder_out}",  "Everything is Ok")
+#def test_step4():
+    #assert chek_func(f"cd {folder_out}; 7z i {folder_ex}",  "Everything is Ok")
 if __name__ == '__main__':
     pytest.main(["-vv"])
