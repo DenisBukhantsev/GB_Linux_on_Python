ceb8f989911f01fff38286624be17f8c1ff2127c
user <old.91@bk.ru>
2023-12-07 22:08:36 +0300

    1

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ result = subprocess.run("cat /etc/os-release", shell=True, stdout=subprocess.PIP
 #out = result.stdout
 #print(out)
 
-def test_func(*args):
-    result = subprocess.run(args, shell=True, stdout=subprocess.PIPE, encoding="utf=8")
-    outt = result.stdout
-    if result.returncode == 0:
-        if "22.04" in outt and "jammy" in outt:
-            return True
-        else:
-            return False
+def test_func(command, text):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, encoding="utf=8")
+    out = result.stdout
+    if result.returncode == 0 and text in out:
+        return True
+    else:
+        return False
+
 
 print(test_func("cat /etc/os-release"))
