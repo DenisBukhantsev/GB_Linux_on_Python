b321448b4ed07e3fb886c095ebd0c5911fbbafb1
DenisBukhantsev <118934730+DenisBukhantsev@users.noreply.github.com>
2024-02-18 16:55:27 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -9,10 +9,10 @@ def test_step1():
     assert chek_func(f"cd {data["folder_in"]}; 7z a {data["folder_out"]}/archive_1",  "Everything is Ok")
 
 def test_step2():
-    assert chek_func(f"cd {folder_out}; 7z d archive_1", "Everything is Ok")
+    assert chek_func(f"cd {data["folder_out"]}; 7z d archive_1", "Everything is Ok")
 
 def test_step3():
-    assert chek_func(f"cd {folder_out}; 7z x {folder_out}",  "Everything is Ok")
+    assert chek_func(f"cd {data["folder_out"]}; 7z x {data["folder_out"]}",  "Everything is Ok")
 #def test_step4():
     #assert chek_func(f"cd {folder_out}; 7z i {folder_ex}",  "Everything is Ok")
 if __name__ == '__main__':
