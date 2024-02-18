e14b4d23020f38cc0be082e16198c10ce80d0dbf
DenisBukhantsev <118934730+DenisBukhantsev@users.noreply.github.com>
2024-02-18 16:53:14 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -1,12 +1,12 @@
 import pytest
+import yaml
 from HW_1 import chek_func
+
 with open('config.yaml') as f:
     data = yaml.safe_load(f)
-folder_in = "/home/user/folder_in"
-folder_out = "/home/user/folder_out"
-folder_ex = "/home/user/folder_ex"
+
 def test_step1():
-    assert chek_func(f"cd {folder_in}; 7z a {folder_out}/archive_1",  "Everything is Ok")
+    assert chek_func(f"cd {data["folder_in"]}; 7z a {data["folder_out"]}/archive_1",  "Everything is Ok")
 
 def test_step2():
     assert chek_func(f"cd {folder_out}; 7z d archive_1", "Everything is Ok")
