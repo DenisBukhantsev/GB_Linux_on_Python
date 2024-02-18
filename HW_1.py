8dfa212ba2f2205357bfed802e877d84dc5a6af5
DenisBukhantsev <118934730+DenisBukhantsev@users.noreply.github.com>
2024-02-18 16:22:01 +0300

    Update HW_1.py

--- HW_1.py
+++ HW_1.py
@@ -11,3 +11,9 @@ def chek_func(command, text):
     else:
         return False
 
+def checkout_negative(command, text):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    if result.returncode != 0 and (text in result.stderr or text in result.stdout):
+        return True
+    else:
+        return False
